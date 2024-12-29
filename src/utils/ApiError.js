8b339ecc4cced40152ee5a4ee869ca896922ee7a
rahul-dhundhwal <dhundhwalcp@gmail.com>
2024-12-29 11:00:54 +0530

    bug fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -9,7 +9,7 @@ class ApiError extends Error{
 
     ){
         super(message)
-        this,statusCode=statusCode
+        this.statusCode=statusCode
         this.data=null
         this.message=message
         this.success=false
